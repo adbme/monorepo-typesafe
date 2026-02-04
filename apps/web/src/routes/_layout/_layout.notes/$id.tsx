6ccd4e16e5336ac312ce74a88985473bc204e03d
adbme <adil.bouamrioune@gmail.com>
2026-02-04 16:17:20 +0100

    fix: ensure note ID is correctly parsed as a number in loader and simplify image rendering logic

--- apps/web/src/routes/_layout/_layout.notes/$id.tsx
+++ apps/web/src/routes/_layout/_layout.notes/$id.tsx
@@ -11,14 +11,14 @@ import notFound from "../../../not-found.svg";
 export const Route = createFileRoute("/_layout/_layout/notes/$id")({
   component: NoteDetail,
   loader: async ({ params: { id } }) => {
-    return await getNote(id);
-  }
+    return await getNote(Number(id)); 
+  },
 });
 
 function NoteDetail() {
   const note = Route.useLoaderData();
 
-console.log("Valeur du contenu :", note);
+  console.log("Valeur du contenu :", note);
 
   if (!note) {
     return (
@@ -46,22 +46,16 @@ console.log("Valeur du contenu :", note);
         <ArrowLeft className="mr-2 h-4 w-4" /> Retour à la liste
       </Link>
 
-      {note.image && (
-        <img
-           src={
-              note.image
-                ? `${webEnv.VITE_API_URL}${note.image}`
-                : notFound
-            }
-          alt={note.title}
-          className="w-full h-64 object-cover rounded-3xl mb-8 shadow-lg"
-          onError={(e) => {
-            const target = e.target as HTMLImageElement;
-            target.onerror = null;
-            target.src = notFound;
-          }}
-        />
-      )}
+      <img
+        src={note.image ? `${webEnv.VITE_API_URL}${note.image}` : notFound}
+        alt={note.title}
+        className="w-full h-64 object-cover rounded-3xl mb-8 shadow-lg"
+        onError={(e) => {
+          const target = e.target as HTMLImageElement;
+          target.onerror = null;
+          target.src = notFound;
+        }}
+      />
 
       <div className="space-y-4">
         <h1 className="text-4xl font-bold tracking-tight">{note.title}</h1>
