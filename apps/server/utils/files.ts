9bc799aa05cc56d338a2fc174db5307fe77ca807
adbme <adil.bouamrioune@gmail.com>
2026-02-04 12:10:31 +0100

    fix: update upload directory and filename formatting in uploadFile function

--- apps/server/utils/files.ts
+++ apps/server/utils/files.ts
@@ -1,12 +1,13 @@
 import { join } from 'node:path'
+import { serverEnv } from "@monorepo-typesafe/env/server";
 
-const UPLOAD_DIR = join(import.meta.dir, '../uploads')
+const UPLOAD_DIR = join(import.meta.dir, '..', serverEnv.UPLOAD_DIR)
 
 export const uploadFile = async (file: File) => {
-  const filename = `${Date.now()}-${file.name}`
+  const filename = `${Date.now()}-${file.name.replace(/\s+/g, '-')}`
   const path = join(UPLOAD_DIR, filename)
   
   await Bun.write(path, file)
   
-  return `/uploads/${filename}`
+  return `${serverEnv.UPLOAD_PREFIX}/${filename}`
 }
